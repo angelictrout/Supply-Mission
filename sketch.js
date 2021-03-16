1359596973bcdc5932188b25c045391f1e804c52
angelictrout <77703377+angelictrout@users.noreply.github.com>
2021-03-15 20:05:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var helicopter1, helicopterSprite, packageSprite,package1;
 var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -7,8 +7,8 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	helicopter1=loadImage("helicopter.png")
+	package1=loadImage("package.png")
 }
 
 function setup() {
@@ -17,30 +17,43 @@ function setup() {
 	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
+	packageSprite.addImage(package1)
 	packageSprite.scale=0.2
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.addImage(helicopter1)
 	helicopterSprite.scale=0.6
 
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	bottom = createSprite(width/2, 650, 200, 20);
+	bottom.shapeColor = "green";
+
+	side1 = createSprite(300, 610, 20, 100);
+	side1.shapeColor = "red";
+
+	side2 = createSprite(500, 610, 20, 100);
+	side2.shapeColor = "red";
+
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , { isStatic:true});
 	World.add(world, packageBody);
+
+	
 	
 
-	//Create a Ground
+	
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
 
 	Engine.run(engine);
+
+	
   
 }
 
@@ -50,16 +63,21 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
+
+  packageSprite.collide(bottom);
+  packageSprite.collide(side1);
+  packageSprite.collide(side2);
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-    Matter.Body.setStatic( packageBody, false);
+	// Look at the hints in the document and understand how to make the package body fall only on
+
+	Matter.Body.setStatic(packageBody, false);
+
+	
+    
   }
 }
-
-
-
